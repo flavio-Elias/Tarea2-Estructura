d00643a0572e397e6f93bcb5af89cb6a7864909b
flavio <flavio2medioa@gmail.com>
2025-04-19 10:53:49 -0400

    Añadí una nota para usar a futuro la funcion qsort() (la verdad esto no aporta mucho, pero es para tener una referencia)

--- grupoX-T2/modules/sorter.c
+++ grupoX-T2/modules/sorter.c
@@ -1,1 +1,4 @@
 #include "sorter.h"
+
+/// sintaxis generica de qsort() (funcion integrada del quicksort) qsort(arr, n, size, comp) , donde arr: es eñ puntero al primer elemento
+// n: es el numero de elementos. size: tamaño de cada elemento. comp: puntero a una funcion que compare.
